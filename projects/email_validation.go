215dc5b4a59c0739bbaa1645e655d9084caaa02b
Hitesh Rana <xs238-hitran@xs-host603-008.xs.neuralcompany.team>
2022-08-22 14:43:25 +0530

    email validation

--- projects/email_validation.go
+++ projects/email_validation.go
@@ -1,8 +1,20 @@
 package projects
 
-import "regexp"
+import (
+	"fmt"
+	"log"
+	"net/mail"
+)
 
-func Email_validation(){
+func ValidMailAddress() {
+	var email string
+	fmt.Print("Enter your email : ")
+	fmt.Scan(&email)
+   _,err := mail.ParseAddress(email)
+   if err != nil{
+	log.Print("invalid email address :", email)
+   }else{
+   log.Print("valid email " , email)
+}
 
-	var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 }
